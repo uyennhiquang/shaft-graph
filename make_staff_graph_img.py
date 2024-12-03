bf008747bcc61416824d437549cc2bf7961e7d42
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2024-12-02 19:25:00 -0500

    Clean up matplot function

--- make_staff_graph_img.py
+++ make_staff_graph_img.py
@@ -9,40 +9,6 @@ import itertools as it
 import matplotlib.pyplot as plt
 from pyvis.network import Network
 
-
-def draw_labeled_multigraph(G, attr_name, ax=None):
-    """
-    Length of connectionstyle must be at least that of a maximum number of edges
-    between pair of nodes. This number is maximum one-sided connections
-    for directed graph and maximum total connections for undirected graph.
-    """
-    # Works with arc3 and angle3 connectionstyles
-    connectionstyle = [f"arc3,rad={r}" for r in it.accumulate([0.15] * 4)]
-    # connectionstyle = [f"angle3,angleA={r}" for r in it.accumulate([30] * 4)]
-
-    pos = nx.shell_layout(G)
-    nx.draw_networkx_nodes(G, pos, ax=ax)
-    nx.draw_networkx_labels(G, pos, font_size=8, ax=ax)
-    nx.draw_networkx_edges(
-        G, pos, edge_color="grey", connectionstyle=connectionstyle, ax=ax
-    )
-
-    labels = {
-        tuple(edge): f"{attr_name}={attrs[attr_name]}"
-        for *edge, attrs in G.edges(keys=True, data=True)
-    }
-    nx.draw_networkx_edge_labels(
-        G,
-        pos,
-        labels,
-        connectionstyle=connectionstyle,
-        label_pos=0.3,
-        font_color="blue",
-        bbox={"alpha": 0},
-        ax=ax,
-    )
-
-
 file = "./new_shows_obj.json"
 with open(file, 'r') as f:
     data = json.load(f)
